6e07b3969cb3fe74a73339dc3ecb2a12788cc3c0
Dan Hintz <dhintz@live.com>
2018-07-24 23:48:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
+  if(cart.length > 0) {
+   for(var i=0; i<cart.length; i++) {
+     var cartList = []
+   } 
+  }else{
+    return 'Your shopping cart is empty.'
+  }
 }
 
 function total() {
